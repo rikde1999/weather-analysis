da62d6749950d972f131c21e9d9ddd1de0a99493
rikde <rikde@gmail.com>
2024-08-23 07:48:52 +0530

    added docker file

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.9
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+RUN mkdir /weather_analysis_system
+WORKDIR /weather_analysis_system
+COPY requirements.txt /weather_analysis_system/
+RUN pip install -r requirements.txt
+COPY . /weather_analysis_system/
+EXPOSE 8000
--- weather_analysis_system/settings.py
+++ weather_analysis_system/settings.py
@@ -81,12 +81,25 @@ WSGI_APPLICATION = "weather_analysis_system.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "weather_analysis",
+        "USER": "postgres",
+        "PASSWORD": "Pa55word!",
+        "HOST": "weather_db",  # This should match the service name in docker-compose
+        "PORT": 5432,
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
