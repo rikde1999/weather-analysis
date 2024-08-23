8669d64451298d0a5a6d45c1799d7e18c743ec84
rikde1999 <rikde1999@gmail.com>
2024-08-23 11:28:14 +0530

    added some static data

--- weather_analysis_system/settings.py
+++ weather_analysis_system/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "/static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
